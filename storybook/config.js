b9c15c31c17f717867a43fefd6937a7f8d8959ec
Diego Adolfo S. de Araujo <daaraujo@uolinc.com>
2019-03-24 16:13:38 -0300

    Configurando storybook e adicionando dependencias

---
+++ storybook/config.js
@@ -0,0 +1,9 @@
+import { configure } from '@storybook/react';
+
+const requireContext = require.context('../src', true, /\.story.jsx?$/);
+
+const _loadStories = () => {
+  requireContext.keys().forEach(story => requireContext(story));
+}
+
+configure(_loadStories, module);
